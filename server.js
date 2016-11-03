cc8c763e5f3e00c2e9e0bcaf7e3f0daa0784fc8f
Sebastian Petak <sebastianpetak@gmail.com>
2016-11-03 11:26:31 -0500

    The application is fully functional.

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-var http = require('http');
-//var useragent = require('useragent');
-//var bodyParser = require('body-parser');
-var server = http.createServer(function(req, res) {
+/*
+	FreeCodeCamp's Request Header Parser Microservice
 	
+	User Story: I can get the IP address, language, and 
+	operating system for my browser.
+*/
 
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+	// ['x-forwarded-for'] will account for proxies, returning
+	// the last proxy the request went through
 	var ip = req.headers['x-forwarded-for'] ||
 		req.connection.remoteAddress ||
 		req.socket.remoteAddress ||
@@ -11,28 +17,15 @@ var server = http.createServer(function(req, res) {
 
 	var language = req.headers["accept-language"];
 
-	var os = req.headers["user-agent"];
-
-	//var os = req.body.urlencoded
-
-	//var os = useragent.parse(req.headers['user-agent']);
-
-	console.log(ip);
-	console.log(language);
-	console.log(os);
-
+	// splits are for isolating the os from the req.header["user-agent"] object
+	var os = req.headers["user-agent"].split(') ')[0].split(' (')[1];
 
 	res.writeHead(200, { "Content-Type": "application/json" });
-
 	res.end(JSON.stringify({
 		"ipaddress": ip,
-		"language": language.match("^.....", ''),
+		"language": language.match("^.....", '')[0],
 		"software": os
 		}));
-
-	res.end()
 })
 
 server.listen(8000);
-
-// any get request we responsd wih IP, language, and OS as a JSON object.
